3ffb059c894059876f10e6eb479bd22be16724d7
Caleb Gardner <calebq23@gmail.com>
2023-04-04 04:17:41 -0500

    Some preperations for adding Stupid backend

--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 class Prefs{
   SharedPreferences prefs;
@@ -14,6 +15,24 @@ class Prefs{
   void setLightTheme(bool p) => prefs.setBool("lightTheme", p);
   bool amoledDark() => prefs.getBool("amoledDark") ?? false;
   void setAmoledDark(bool p) => prefs.setBool("amoledDark", p);
+
+  // Stupid
+  Future<String> stupidUuid() async {
+    var id = await securePrefs.read(key: "uuid");
+    if(id == null){
+      id = const Uuid().v4();
+      await securePrefs.write(key: "uuid", value: id);
+    }
+    return id;
+  }
+  bool log() => prefs.getBool("stupidLog") ?? true;
+  void setLog(bool p) => prefs.setBool("stupidLog", p);
+  bool crash() => prefs.getBool("stupidCrash") ?? true;
+  void setCrash(bool p) => prefs.setBool("stupidCrash", p);
+  Future<String?> username() => securePrefs.read(key: "stupidUsername");
+  Future<void> setUsername(String u) => securePrefs.write(key: "stupidUsername", value: u);
+  Future<String?> password() => securePrefs.read(key: "stupidPassword");
+  Future<void> setPassword(String p) => securePrefs.write(key: "stupidPassword", value: p);
 
   // Misc
   bool disableAnimations() => prefs.getBool("DisableAnimations") ?? false;
